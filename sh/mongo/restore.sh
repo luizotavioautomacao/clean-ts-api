e7cb2813e90fb668bdbe000152e0f38dc042c36e
Luiz Otávio <luizotavioautomacao@gmail.com>
2024-08-28 17:27:13 -0300

    chore: restore 1 file at a time

--- sh/mongo/restore.sh
+++ sh/mongo/restore.sh
@@ -4,9 +4,20 @@ set -e # if the script fail this command will out of script
 
 source .env ## to read .env file
 
-# docker-compose up -d mongo
-# sleep 10
+# docker exec -it mongo-container mongorestore --batchSize=10 --numInsertionWorkersPerCollection 1 -u $MONGO_USER -p $MONGO_PASSWORD --db $MONGO_DB_NAME $MONGO_BACKUP_FOLDER --drop --noIndexRestore # backup into container
 
-docker exec -it mongo-container mongorestore --batchSize=10 --numInsertionWorkersPerCollection 1 -u $MONGO_USER -p $MONGO_PASSWORD --db $MONGO_DB_NAME $MONGO_BACKUP_FOLDER --drop --noIndexRestore # backup into container
+# Itera sobre cada arquivo BSON no diretório de backup
+for file in ${MONGO_BACKUP_FOLDER}/*.bson; do
+  # Extrai o nome da coleção a partir do nome do arquivo
+  collection=$(basename "$file" .bson)
+  
+  echo "Restaurando a coleção: $collection"
+  
+  # Executa o comando mongorestore para restaurar a coleção
+  docker exec -it mongo-container mongorestore --batchSize=10 --numInsertionWorkersPerCollection 1 \
+    -u $MONGO_USER -p $MONGO_PASSWORD --db $MONGO_DB_NAME --collection $collection --drop --noIndexRestore "$file"
+  
+  echo "Coleção $collection restaurada."
+done
 
-# docker-compose down
+echo "Restauração completa."
